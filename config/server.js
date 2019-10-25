b664df07cb76cb82d459536d05ea08494482a466
vtranduc <vtranduc@ryerson.ca>
2019-10-25 11:42:56 -0400

    done

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = {
+  ACCESSKEY:
+    process.env.ACCESSKEY ||
+    "45745d541be47d739f31eb7d36670089947810c936a88f894c56e2ff6951851f",
+  SECRET:
+    process.env.SECRET ||
+    "ae1e87f6a1a0a557830800955cb753a912cdb0e140bcacd12ad589b620a7ee49",
+  CALLBACK_URL: process.env.CALLBACK_URL || "http://localhost:3000"
+};
